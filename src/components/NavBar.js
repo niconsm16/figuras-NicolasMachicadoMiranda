6e46ec8c33c1a026b31e4881c6baaf75109ad4f6
niconsm <supercuate16@gmail.com>
2022-03-08 19:10:52 -0300

    strComponents

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,40 +1,42 @@
 import React from "react";
 import * as Styled from "./NavBarElements";
 import { FaStore, FaHome, FaGavel, FaGlobeAmericas, FaAngleDoubleDown, FaUserAlt, FaPlus } from "react-icons/fa";
+import * as Menu from "./NavStr";
 
 const NavBar = () => {
     return (
         <Styled.MenuContainer className="absolute w-full text-xl">
             <Styled.MenuWrapper className="flex w-full pl-10 h-16 bg-black absolute top-0 text-white">
                 <Styled.MenuLogo className="flex justify-start items-center w-2/6 font-bakbak text-amarillo">
-                    <FaStore className="mx-3 h-8 w-8 text-white" /> Mi Marketplace
+                    <FaStore className="mx-3 h-8 w-8 text-white" />
+                    {Menu.Market}
                 </Styled.MenuLogo>
                 <Styled.Menu className="flex w-full items-center justify-end antialised font-ptsans font-medium">
                     <Styled.MenuLI className="flex h-full items-center">
                         <Styled.MenuA>
                             <FaHome style={{ paddingRight: 3, marginRight: 2 }} />
-                            Inicio
+                            {Menu.Home}
                         </Styled.MenuA>
                         <Styled.MenuA>
                             <FaAngleDoubleDown style={{ paddingRight: 3, marginRight: 2 }} />
-                            Ingresos
+                            {Menu.Ingresos}
                         </Styled.MenuA>
                         <Styled.MenuA>
                             <FaGlobeAmericas style={{ paddingRight: 3, marginRight: 2 }} />
-                            Los + Buscados
+                            {Menu.Buscados}
                         </Styled.MenuA>
                         <Styled.MenuA>
                             <FaPlus style={{ paddingRight: 3, marginRight: 2 }} />
-                            Solicitados
+                            {Menu.Solicitados}
                         </Styled.MenuA>
                         <Styled.MenuA>
                             <FaGavel style={{ paddingRight: 3, marginRight: 2 }} />
-                            Pujas
+                            {Menu.Pujas}
                         </Styled.MenuA>
                         <Styled.MenuProfile className="flex h-full items-center">
                             <Styled.MenuProfileA>
                                 <FaUserAlt style={{ paddingRight: 3, marginRight: 2 }} />
-                                Mi Perfil
+                                {Menu.Profile}
                             </Styled.MenuProfileA>
                         </Styled.MenuProfile>
                     </Styled.MenuLI>
---
+++ src/components/NavStr.js
@@ -0,0 +1,7 @@
+export const Market = 'Mi Marketplace'
+export const Home = 'Inicio'
+export const Ingresos = 'Ingresos'
+export const Buscados = 'Los + Buscados'
+export const Solicitados = 'Solicitados'
+export const Pujas = 'Pujas'
+export const Profile = 'Mi Perifl'
