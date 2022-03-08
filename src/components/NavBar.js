2f7714d3116d30d92579f284dd7f651deda5bf98
niconsm <supercuate16@gmail.com>
2022-03-08 18:09:56 -0300

    alias added

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,47 +1,47 @@
 import React from "react";
-import { Container, LogoM, Menu, MenuI, MenuIL, Profile, ProfileA, Wrapper } from "./NavBarElements";
+import * as Styled from "./NavBarElements";
 import { FaStore, FaHome, FaGavel, FaGlobeAmericas, FaAngleDoubleDown, FaUserAlt, FaPlus } from "react-icons/fa";
 
 const NavBar = () => {
     return (
-        <Container className="absolute w-full text-xl">
-            <Wrapper className="flex w-full pl-10 h-16 bg-black absolute top-0 text-white">
-                <LogoM className="flex justify-start items-center w-2/6 font-bakbak text-amarillo">
+        <Styled.Container className="absolute w-full text-xl">
+            <Styled.Wrapper className="flex w-full pl-10 h-16 bg-black absolute top-0 text-white">
+                <Styled.LogoM className="flex justify-start items-center w-2/6 font-bakbak text-amarillo">
                     <FaStore className="mx-3 h-8 w-8 text-white" /> Mi Marketplace
-                </LogoM>
-                <Menu className="flex w-full items-center justify-end antialised font-ptsans font-medium">
-                    <MenuI className="flex h-full items-center">
-                        <MenuIL>
+                </Styled.LogoM>
+                <Styled.Menu className="flex w-full items-center justify-end antialised font-ptsans font-medium">
+                    <Styled.MenuI className="flex h-full items-center">
+                        <Styled.MenuIL>
                             <FaHome style={{ paddingRight: 3, marginRight: 2 }} />
                             Inicio
-                        </MenuIL>
-                        <MenuIL>
+                        </Styled.MenuIL>
+                        <Styled.MenuIL>
                             <FaAngleDoubleDown style={{ paddingRight: 3, marginRight: 2 }} />
                             Ingresos
-                        </MenuIL>
-                        <MenuIL>
+                        </Styled.MenuIL>
+                        <Styled.MenuIL>
                             <FaGlobeAmericas style={{ paddingRight: 3, marginRight: 2 }} />
                             Los + Buscados
-                        </MenuIL>
-                        <MenuIL>
+                        </Styled.MenuIL>
+                        <Styled.MenuIL>
                             <FaPlus style={{ paddingRight: 3, marginRight: 2 }} />
                             Solicitados
-                        </MenuIL>
-                        <MenuIL>
+                        </Styled.MenuIL>
+                        <Styled.MenuIL>
                             <FaGavel style={{ paddingRight: 3, marginRight: 2 }} />
                             Pujas
-                        </MenuIL>
-                        <Profile className="flex h-full items-center">
-                            <ProfileA>
+                        </Styled.MenuIL>
+                        <Styled.Profile className="flex h-full items-center">
+                            <Styled.ProfileA>
                                 <FaUserAlt style={{ paddingRight: 3, marginRight: 2 }} />
                                 Mi Perfil
-                            </ProfileA>
-                        </Profile>
-                    </MenuI>
-                </Menu>
+                            </Styled.ProfileA>
+                        </Styled.Profile>
+                    </Styled.MenuI>
+                </Styled.Menu>
 
-            </Wrapper>
-        </Container>
+            </Styled.Wrapper>
+        </Styled.Container>
     )
 };
 export default NavBar;
