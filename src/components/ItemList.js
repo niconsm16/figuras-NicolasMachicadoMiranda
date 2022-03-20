5dc1ec73a975b67c2927944d75e2277e95ea4742
niconsm <supercuate16@gmail.com>
2022-03-19 22:00:29 -0300

    sync mock working

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,49 +1,41 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Item from "./Item";
 
-const productos = require('./Productos')
+// Preparando la promesa
 
 let waitDb = () => {
-
+    const productos = require('./Productos')
     return new Promise((resolve, reject) => {
-
         setTimeout(() => {
-
             productos.figures !== null ? resolve(console.log('DB con datos! ')) : reject(console.log('DB vacio'))
         }, 2000)
     })
 }
-waitDb()
-
-    .then(() => {
-        console.log('hola')
-    })
-    .catch((err) => {
-        return (
-            <div>No se pudo cargar los productos: {err}</div>
-        )
-    })
-
 
 
 const ItemList = () => {
 
-    let [products, setProducts] = useState(productos.figures)
+    let [products, setProducts] = useState([]) // Seteo el estado inicial vacío en array
+
+    useEffect(() => { // Mantenemos vigilado todo el tiempo los eventos para actualizar la data una vez llegada
+        waitDb() // Ejecuto la demora simulando un servidor
+            .then(() => {
+                const productos = require('./Productos') // utilizo el require como si fuese un fetch o axios
+                setProducts(productos.figures) // Actualizo valores con los cargados en la línea de arriba
+            })
+            .catch((err) => {
+                console.log('No se pudo recibir la información: ', err)
+            })
+    }, [])
 
     return (
         <div className="flex justify-center text-center m-6 select-none flex-wrap">
-            {
-                products.map((n) => {
-                    return (
-                        <Item key={n.id} url={n.url} name={n.name} details={n.details} price={n.price} />
-                    )
-                })
+            {products.map((n) => { // Mapeamos todos los productos
+                return (<Item key={n.id} url={n.url} name={n.name} details={n.details} price={n.price} />)
+            })
             }
         </div>
     )
 }
-
-
-
 
 export default ItemList
