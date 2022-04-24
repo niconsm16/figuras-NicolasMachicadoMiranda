ede3f0b17974cead892fb0aa651290a97b0f535f
niconsm <supercuate16@gmail.com>
2022-04-23 22:32:24 -0300

    Proyecto Final controlStock

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,9 +1,10 @@
 import { useContext, useState } from "react"
 import { CartContext } from "../Cart/CartContext"
 import { figuresdb } from '../../firebase/config.js'
-import { collection, addDoc, Timestamp } from 'firebase/firestore'
-import { Navigate, Link } from "react-router-dom"
+import { collection, addDoc, documentId, getDocs, Timestamp, writeBatch, query, where } from 'firebase/firestore'
+import { Navigate } from "react-router-dom"
 import { motion } from "framer-motion"
+import { Success } from "./Success"
 
 
 const Checkout = () => {
@@ -36,7 +37,7 @@ const Checkout = () => {
         })
     }
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault()
         const orden = {
             items: cart,
@@ -67,12 +68,41 @@ const Checkout = () => {
             }
         }
 
+
+        const batch = writeBatch(figuresdb)
+        const productsRef = collection(figuresdb, 'productos')
         const ordersRef = collection(figuresdb, 'ordenes')
-        addDoc(ordersRef, orden)
-            .then((res) => {
-                setOrderId(res.id)
-                emptyCart()
-            })
+
+        const q = query(productsRef, where(documentId(), 'in', cart.map(prod => prod.id)))
+
+        const productos = await getDocs(q)
+
+        const outOfStock = []
+
+        productos.docs.forEach(prod => {
+            const findout = cart.find(found => found.id === prod.id)
+            if (prod.data().stock >= findout.count) {
+                batch.update(prod.ref, {
+                    stock: prod.data().stock - findout.count
+                })
+            } else {
+                outOfStock.push(findout)
+            }
+        })
+
+        if (outOfStock.length === 0) {
+            batch.commit()
+            addDoc(ordersRef, orden)
+                .then((res) => {
+                    setOrderId(res.id)
+                    emptyCart()
+                })
+        } else {
+            console.log(outOfStock)
+            console.log('nanana')
+        }
+
+
     }
 
     // Variantes de motion
@@ -120,12 +150,7 @@ const Checkout = () => {
 
     // Tailwind Css
 
-    // Concretado
-    const cSuccessContainer = "flex flex-col h-screen justify-around pt-28 px-12 items-center bg-lime-300"
-    const cSuccessTitle = "font-fredoka text-8xl text-black plusfonts animate-bounce"
-    const cIdMessage = 'my-4 px-6 pt-2 pb-3 bg-black text-white text-2xl rounded-full z-10'
-    const cWave = 'absolute bottom-0 w-full bg-transparent'
-    const cBack = "py-2 px-4 bg-amarillo rounded font-bold hover:bg-black hover:text-amarillo transition duration-300 z-10"
+
     // Checkout
     const cContainer = "flex flex-col h-screen px-12 items-center bg-white"
     const cTitle = "my-24 text-8xl color-black font-guy plusfonts"
@@ -140,33 +165,7 @@ const Checkout = () => {
     // Fase de Transacción exitosa
 
     if (orderId) {
-        return <div className={cSuccessContainer}>
-            <h2 className={cSuccessTitle}>Transacción exitosa!</h2>
-
-            <h2 className={cIdMessage}>Este es el ID de tu transacción: <b>{orderId}</b></h2>
-
-            <motion.div className="z-10"
-                initial={{ y: 0, scale: 1 }}
-                transition={{ duration: .25 }}
-                whileTap={{ y: -500, scale: 50 }}
-            >
-                <Link to='/' className={cBack}>
-                    Volver al inicio
-                </Link>
-            </motion.div>
-            <div className={`${cWave} fill-lime-400`}>
-                <svg viewBox="0 0 500 150" preserveAspectRatio="none" >
-                    <path d="M-40.34,82.41 C157.16,-77.44 207.95,126.81 573.08,-18.23 L533.01,165.30 L0.00,150.00 Z" >
-                    </path>
-                </svg>
-            </div>
-            <div className={`${cWave} fill-lime-500`}>
-                <svg viewBox="0 0 500 150" preserveAspectRatio="none" >
-                    <path d="M-47.11,173.20 C141.92,-87.31 231.65,124.84 574.77,31.09 L533.01,165.30 L0.00,150.00 Z" >
-                    </path>
-                </svg>
-            </div>
-        </div>
+        return <Success id={orderId} />
     }
 
     if (cart.length === 0) {
---
+++ src/components/Checkout/Success.js
@@ -0,0 +1,42 @@
+import { motion } from "framer-motion"
+import { Link } from "react-router-dom"
+
+export const Success = ({ id }) => {
+
+    // Tailwind CSS
+    const cSuccessContainer = "flex flex-col h-screen justify-around pt-28 px-12 items-center bg-lime-300"
+    const cSuccessTitle = "font-fredoka text-8xl text-black plusfonts animate-bounce"
+    const cIdMessage = 'my-4 px-6 pt-2 pb-3 bg-black text-white text-2xl rounded-full z-10'
+    const cWave = 'absolute bottom-0 w-full bg-transparent'
+    const cBack = "py-2 px-4 bg-amarillo rounded font-bold hover:bg-black hover:text-amarillo transition duration-300 z-10"
+
+    return (
+        <div className={cSuccessContainer}>
+            <h2 className={cSuccessTitle}>Transacción exitosa!</h2>
+
+            <h2 className={cIdMessage}>Este es el ID de tu transacción: <b>{id}</b></h2>
+
+            <motion.div className="z-10"
+                initial={{ y: 0, scale: 1 }}
+                transition={{ duration: .25 }}
+                whileTap={{ y: -500, scale: 50 }}
+            >
+                <Link to='/' className={cBack}>
+                    Volver al inicio
+                </Link>
+            </motion.div>
+            <div className={`${cWave} fill-lime-400`}>
+                <svg viewBox="0 0 500 150" preserveAspectRatio="none" >
+                    <path d="M-40.34,82.41 C157.16,-77.44 207.95,126.81 573.08,-18.23 L533.01,165.30 L0.00,150.00 Z" >
+                    </path>
+                </svg>
+            </div>
+            <div className={`${cWave} fill-lime-500`}>
+                <svg viewBox="0 0 500 150" preserveAspectRatio="none" >
+                    <path d="M-47.11,173.20 C141.92,-87.31 231.65,124.84 574.77,31.09 L533.01,165.30 L0.00,150.00 Z" >
+                    </path>
+                </svg>
+            </div>
+        </div>
+    )
+}
